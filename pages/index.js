bbae9aca6a3e68414d23b8bedd2fe00a826dfc70
Niwatchai Wangtrakuldee <niwatchai.wang@mail.kmutt.ac.th>
2022-04-27 00:22:21 +0700

    First Deploy

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="card">
-        <p>WOWZZA</p>
+    <div className={styles.fix960}>
+      <div className={styles.slide}>
+        <img src='https://tinyurl.com/yxp65vwr' className='img-fluid w-100  ' alt='test'></img>
+      </div>
+      <h4>Wowza007</h4>
     </div>
   )
 }
