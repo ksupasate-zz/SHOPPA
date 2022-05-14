4427f5838cc70769095dac88168f4d33e43fa5de
Tanasead Rattanapan <misstergongz09@gmail.com>
2022-05-14 19:41:08 +0700

    Footer update

--- pages/comps/Footer.js
+++ pages/comps/Footer.js
@@ -1,9 +1,41 @@
-const Footer = () => {
-    return (  
-        <div>
-            By Shoppa
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.footer}>
+          <div className={styles.table}>
+        <table>
+          <tr>
+            <th className={styles.head}>BUY</th>
+            <th className={styles.head}>SELL</th>
+            <th className={styles.head}>CUSTOMER SUPPORT</th>
+          </tr>
+          <tr>
+            <td><a className={styles.td} href="#">Buyer Manual</a></td>
+            <td><a className={styles.td} href="#">Seller Manual</a></td>
+            <td><a className={styles.td} href="#">Help Center</a></td>
+          </tr>
+          <tr>
+             <td><a className={styles.td} href="#">Authentication</a></td>
+             <td><a className={styles.td} href="#">Seller Tier</a></td>
+             <td><a className={styles.td} href="#">FAQ</a></td>
+          </tr>
+          <tr>
+            <td><a className={styles.td} href="#">Sasom Services</a></td>
+            <td><a className={styles.td} href="#">Sasom Product Guideline</a></td>
+            <td><a className={styles.td} href="#">Contact Us</a></td>
+          </tr>
+        </table>
+        <hr className={styles.linedes}></hr>
         </div>
-    );
+        <div className={styles.last}>
+        <p className={styles.td}>&copy; 2022 COPYRIGHT | SHOPPA</p>
+        <p className={styles.td}>Term & Condition</p>
+        <p className={styles.td}>Privacy Policy</p>
+        </div>
+      </div>
+    </main>
+  );
 }
- 
-export default Footer;
--- pages/main.js
+++ pages/main.js
@@ -4,6 +4,7 @@ import Navbar from "./comps/Navbar";
 import { Carousel } from "antd";
 import { Card } from "antd";
 import Link from "next/link";
+import Footer from "./comps/Footer";
 
 const { Meta } = Card;
 
@@ -30,75 +31,91 @@ export default function Home() {
           <h2>NEW ARRIVALS</h2>
         </div>
         <div>
-          <Card className={styles.card}
+          <Card
+            className={styles.card}
             hoverable
             style={{ width: 300 }}
             cover={
               <Link href="/Product?id=P0000007">
-              <img
-                alt="example"
-                src="https://cf.shopee.co.th/file/9b44066d2ddd6db2a39d0a9c2248b634"
-              />
+                <img
+                  alt="example"
+                  src="https://cf.shopee.co.th/file/9b44066d2ddd6db2a39d0a9c2248b634"
+                />
               </Link>
             }
           >
             <Meta title="Air Jordan 1 Retro UNC" description="6400 ฿" />
           </Card>
-          <Card className={styles.card}
+          <Card
+            className={styles.card}
             hoverable
             style={{ width: 300 }}
             cover={
               <Link href="/Product?id=P0000008">
-              <img
-                alt="example"
-                src="https://cf.shopee.co.th/file/77ae064ea246fbeba9b64f4faa4d0ff7"
-              />
+                <img
+                  alt="example"
+                  src="https://cf.shopee.co.th/file/77ae064ea246fbeba9b64f4faa4d0ff7"
+                />
               </Link>
             }
           >
             <Meta title="Air Jordan 1 Mid Chicago Toe" description="4200 ฿" />
           </Card>
-          <Card className={styles.card}
+          <Card
+            className={styles.card}
             hoverable
             style={{ width: 300 }}
             cover={
               <Link href="/Product?id=P0000009">
-              <img
-                alt="example"
-                src="https://cf.shopee.co.th/file/4bd5094d47d3922fb1131efb8b1fdaef"
-              />
+                <img
+                  alt="example"
+                  src="https://cf.shopee.co.th/file/4bd5094d47d3922fb1131efb8b1fdaef"
+                />
               </Link>
             }
           >
-            <Meta title="Air Jordan 1 Retro High OG Sail" description="5800 ฿"/>
+            <Meta
+              title="Air Jordan 1 Retro High OG Sail"
+              description="5800 ฿"
+            />
           </Card>
         </div>
         <div className={styles.new}>
-        <button className={styles.view} type="button">
-              View More
-            </button> 
+          <button className={styles.view} type="button">
+            View More
+          </button>
         </div>
         <div>
-            <div className={styles.our}>
-                <a href="#"><img src="https://i.pinimg.com/736x/2c/a0/2a/2ca02a6531b013f104bafa55061882b4.jpg"></img></a>
-                <div className={styles.center}>Our Story</div>
-            </div>
-            <div className={styles.our}>
-                <a href="#"><img src="https://i.pinimg.com/736x/2c/a0/2a/2ca02a6531b013f104bafa55061882b4.jpg"></img></a>
-                <div className={styles.center}>Our Blog</div>
-            </div>
-            <div className={styles.cate}>
-                <a href="/Shoes"><img src="https://cf.shopee.co.th/file/a4d6f08393112649b3d2627c14ef1d8c"></img></a>
-                <div className={styles.center}>Shoes</div>
-            </div>
-            <div className={styles.cate}>
-                <a href="#"><img src="https://cf.shopee.co.th/file/ff3aa71da5e64f48e3b049599cf1f659"></img></a>
-                <div className={styles.center}>Apparel</div>
-            </div>
-            <div className={styles.cate}>
-                <a href="#"><img src="https://cf.shopee.co.th/file/d0f1760a963499508a61d45fe6c52fda"></img></a> 
-                <div className={styles.center}>Accessories</div>
-            </div>
+          <div className={styles.our}>
+            <a href="#">
+              <img src="https://i.pinimg.com/736x/2c/a0/2a/2ca02a6531b013f104bafa55061882b4.jpg"></img>
+            </a>
+            <div className={styles.center}>Our Story</div>
+          </div>
+          <div className={styles.our}>
+            <a href="#">
+              <img src="https://i.pinimg.com/736x/2c/a0/2a/2ca02a6531b013f104bafa55061882b4.jpg"></img>
+            </a>
+            <div className={styles.center}>Our Blog</div>
+          </div>
+          <div className={styles.cate}>
+            <a href="/Shoes">
+              <img src="https://cf.shopee.co.th/file/a4d6f08393112649b3d2627c14ef1d8c"></img>
+            </a>
+            <div className={styles.center}>Shoes</div>
+          </div>
+          <div className={styles.cate}>
+            <a href="#">
+              <img src="https://cf.shopee.co.th/file/ff3aa71da5e64f48e3b049599cf1f659"></img>
+            </a>
+            <div className={styles.center}>Apparel</div>
+          </div>
+          <div className={styles.cate}>
+            <a href="#">
+              <img src="https://cf.shopee.co.th/file/d0f1760a963499508a61d45fe6c52fda"></img>
+            </a>
+            <div className={styles.center}>Accessories</div>
+          </div>
         </div>
       </div>
     </main>
