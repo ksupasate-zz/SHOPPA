d12f73f1ee4a36dbf240effe0e52d712aabce9a9
nwtkd <75906754+nwtkd@users.noreply.github.com>
2022-05-16 05:56:31 +0700

    Update selectbank.js

--- pages/comps/selectbank.js
+++ pages/comps/selectbank.js
@@ -1,48 +1,20 @@
 import Card from "@mui/material/Card";
-import styles from "../../styles/Puth.module.css";
 import BasicSelect from "./selectbankcomp";
 import * as React from "react";
 
 import {
-  CardContent,
   Grid,
-  TextField,
-  Button,
   createTheme,
-  ThemeProvider,
-  Typography,
 } from "@mui/material";
 export default function Selectbank() {
 
-  
-
   return (
     <Card>
-      <CardContent>
-        {/* <Typography gutterBottom variant="h4">
-            Select Bank
-          </Typography> */}
-       
+        <form action="" method="post">
           <Grid xs={12} item>
             <BasicSelect></BasicSelect>
           </Grid>
-          {/* <Grid xs={12} item>
-                <ThemeProvider theme={theme}>
-                  <Button
-                    onClick={uploadToServer}
-                    type="submit"
-                    variant="contained"
-                    color="neutral"
-                    fullWidth
-                  >
-                    Submit
-                  </Button>
-                  </ThemeProvider>
-            </Grid> */}
         </form>
-
-
-      </CardContent>
     </Card>
   );
 }
